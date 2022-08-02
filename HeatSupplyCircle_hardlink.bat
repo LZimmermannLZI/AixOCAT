84f49e89b46e8daf435d43b5b43f47d461e1af22
LZimmermannLZI <lukas.zimmermann1@rwth-aachen.de>
2022-08-02 15:31:14 +0200

    Update bat file with exact path

--- HeatSupplyCircle_hardlink.bat
+++ HeatSupplyCircle_hardlink.bat
@@ -5,7 +5,7 @@ title Make hard links from project to test project
 echo This script will make hard links of the project files in the POUs folder to the test project folder.
 echo Before running this script make sure you already created the neccesarry folder structure in your Plc or TwinCAT project.
 
-for %%i in (.\AixOCAT\Examples\HeatSupplyCircle\HeatingSupplySystem\ProgrammHalle\POUs\*.TcPOU) do (
+for %%i in (.\AixOCAT\Examples\HeatSupplyCircle\HeatingSupplySystem\ProgrammHalle\POUs\Components\*.TcPOU) do (
 	mklink /H .\AixOCAT\Examples\Tests\TcUnit_Tests\TcUnit_Tests\TcUnit_Tests\POUs\%%~nxi %%i
 )
 pause
