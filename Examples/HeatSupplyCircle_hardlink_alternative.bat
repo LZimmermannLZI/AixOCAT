a4722387cf92c9f97c1b921efe6be22e091060c1
Rando <lukas.zimmermann@eonerc.rwth-aachen.de>
2022-08-04 14:51:43 +0200

    Reedit bat files to address local repo

--- Examples/HeatSupplyCircle_hardlink_alternative.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-title Make hard links from project to test project
-
-echo This script will make hard links of the project files in the POUs folder to the test project folder.
-echo Before running this script make sure you already created the neccesarry folder structure in your Plc or TwinCAT project.
-
-fsutil hardlink create .\Tests\TcUnit_Tests\TcUnit_Tests\TcUnit_Tests\POUs\FB_FanControl.TcPOU .\HeatSupplyCircle\HeatingSupplySystem\ProgrammHalle\POUs\Components\FB_FanControl.TcPOU
-fsutil hardlink create .\Tests\TcUnit_Tests\TcUnit_Tests\TcUnit_Tests\POUs\FC_HeatingCurve.TcPOU .\HeatSupplyCircle\HeatingSupplySystem\ProgrammHalle\POUs\Components\FC_HeatingCurve.TcPOU
--- Examples/HeatSupplyCircle_links_DELETE.bat
+++ Examples/HeatSupplyCircle_links_DELETE.bat
@@ -4,6 +4,6 @@ title Delete hard links from project to test project
 
 echo This script will delete hard links in the POUs folder of the test project folder.
 
-for %%i in (C:\actions-runner\_work\AixOCAT\AixOCAT\Examples\Tests\TcUnit_Tests\TcUnit_Tests\TcUnit_Tests\POUs) do (
+for %%i in (.\Tests\TcUnit_Tests\TcUnit_Tests\TcUnit_Tests\POUs) do (
 	del /Q %%i
 )
