077845230b819e26f2be1bbe49d7cbb6a59b39c0
Rando <lukas.zimmermann@eonerc.rwth-aachen.de>
2022-08-04 14:21:20 +0200

    add hardlink bat files to repo

--- Examples/HeatSupplyCircle_hardlink.bat
+++ Examples/HeatSupplyCircle_hardlink.bat
@@ -5,6 +5,6 @@ title Make hard links from project to test project
 echo This script will make hard links of the project files in the POUs folder to the test project folder.
 echo Before running this script make sure you already created the neccesarry folder structure in your Plc or TwinCAT project.
 
-for %%i in (.\HeatSupplyCircle\HeatingSupplySystem\ProgrammHalle\POUs\Components\*.TcPOU) do (
-	mklink /h .\Tests\TcUnit_Tests\TcUnit_Tests\TcUnit_Tests\POUs\%%~nxi %%i
+for %%i in (C:\actions-runner\_work\AixOCAT\AixOCAT\Examples\HeatSupplyCircle\HeatingSupplySystem\ProgrammHalle\POUs\Components\*.TcPOU) do (
+	mklink /h C:\actions-runner\_work\AixOCAT\AixOCAT\Examples\Tests\TcUnit_Tests\TcUnit_Tests\TcUnit_Tests\POUs\%%~nxi %%i
 )
